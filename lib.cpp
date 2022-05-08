b8846bf703fa96a9a958b888fc0b10fd631a489c
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-08 15:20:40 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include "lib.h"
+int controllo(char a)
+{
+    int b;
+    if((int)a>=65 && (int)a<=90)
+    {
+        b=(int)a+32;
+
+    }else if((int)a>=97 && (int)a<=122)
+    {
+        b=(int)a-32;
+    }
+    return b;
+}
