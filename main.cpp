a15af361f5ab334cd0b81247b882e212b6a580a8
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-08 15:21:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+
+int main()
+{
+    char a;;
+    cin>>a;
+    int b=controllo(a);
+    if(b>=65 &&b<=90)
+    {
+        cout<< (char)b<<endl;
+    }else if(b>=97 && b<=122)
+    {
+        cout<< (char)b<<endl;
+    }else
+    {
+      cout<<"errore"<<endl;
+    }
+
+    return 0;
 }
+
